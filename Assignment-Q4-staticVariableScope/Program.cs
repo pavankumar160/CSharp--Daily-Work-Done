f32b60b0a9b1b35221a32aa5c1482823ec3ccc4e
= <=>
2025-08-18 10:56:08 +0530

    Assignment Questions

---
+++ Assignment-Q4-staticVariableScope/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Assignment_Q4_staticVariableScope
+{
+    class Program
+    {
+        public static void Main()
+        {
+            staticVariableScope object1 = new staticVariableScope();
+            staticVariableScope object2 = new staticVariableScope();
+            staticVariableScope object3 = new staticVariableScope();
+            staticVariableScope object4 = new staticVariableScope();
+        }
+    }
+}
---
+++ Assignment-Q4-staticVariableScope/staticVariableScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assignment_Q4_staticVariableScope
+{
+    
+    class staticVariableScope
+    {   
+        //static variable...
+        static public int staticVar = 10;
+
+        //Non static Variable...
+        public int nonStaticVar = 20;
+
+        public staticVariableScope()
+        {
+            staticVar++;
+            Console.WriteLine($"staticVar = {staticVar}");
+            nonStaticVar++;
+            Console.WriteLine($"nonStaticVar = {nonStaticVar}");
+        }
+    }
+}
